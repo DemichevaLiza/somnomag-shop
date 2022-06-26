bebdbec9caae45144065b2757a73d6044e2d294e
Liza Demicheva <90695376+DemichevaLiza@users.noreply.github.com>
2022-06-26 22:34:10 +0300

    8th

--- js/main.js
+++ js/main.js
@@ -5,28 +5,28 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.shop').hover(function() {
     $('.shapemain').toggleClass('shapemain-off');
     $('.shape1').toggleClass('shape1-on');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.event').hover(function() {
     $('.shapemain').toggleClass('shapemain-off');
     $('.shape2').toggleClass('shape2-on');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.contacts').hover(function() {
     $('.shapemain').toggleClass('shapemain-off');
     $('.shape3').toggleClass('shape3-on');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.history').click(function() {
     $('.shape4a').css('display', 'none');
     $('.texthistory').css('display', 'block');
@@ -35,7 +35,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.dreams').click(function() {
     $('.shape4a').css('display', 'none');
     $('.texthistory').css('display', 'none');
@@ -44,7 +44,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.slide1').click(function() {
     $('.points').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -55,21 +55,21 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.info').click(function() {
     $('.blockinfo1').fadeIn();
     $('.close').css('display', 'block');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close').click(function() {
     $('.blockinfo1').fadeOut();
     $('.close').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.arrow').click(function() {
     $('.points').css('display', 'flex');
     $('.containermain').css('display', 'grid');
@@ -101,7 +101,7 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
+$(function() {
   $('.slide2').click(function() {
     $('.points').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -112,63 +112,63 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.info2').click(function() {
     $('.blockinfo2').fadeIn();
     $('.close2').css('display', 'block');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close2').click(function() {
     $('.blockinfo2').fadeOut();
     $('.close2').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.info3').click(function() {
     $('.blockinfo3').fadeIn();
     $('.close3').css('display', 'block');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close3').click(function() {
     $('.blockinfo3').fadeOut();
     $('.close3').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.info4').click(function() {
     $('.blockinfo4').fadeIn();
     $('.close4').css('display', 'block');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close4').click(function() {
     $('.blockinfo4').fadeOut();
     $('.close4').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.info5').click(function() {
     $('.blockinfo5').fadeIn();
     $('.close5').css('display', 'block');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close5').click(function() {
     $('.blockinfo5').fadeOut();
     $('.close5').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.slide3').click(function() {
     $('.points').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -179,7 +179,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.slide4').click(function() {
     $('.points').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -190,7 +190,7 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.slide5').click(function() {
     $('.points').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -201,65 +201,81 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.fordream').click(function() {
     $('.shape1a').css('display', 'none');
     $('.dreamsproducts').css('display', 'grid');
     $('.vector').css('display', 'block');
+    $('.vector4').css('display', 'none');
+    $('.souvenirproducts').css('display', 'none');
+    $('.texthistory1').css('display', 'none');
+    $('.arrowdown').css('display', 'none');
+    $('.arrowdownwhite').css('display', 'none');
+    $('.book1').css('display', 'none');
+    $('.winx').css('display', 'none');
+  });
+  $('.souvenir').click(function() {
+    $('.shape1a').css('display', 'none');
+    $('.souvenirproducts').css('display', 'grid');
+    $('.vector').css('display', 'none');
+    $('.vector4').css('display', 'block');
+    $('.dreamsproducts').css('display', 'none');
+    $('.texthistory1').css('display', 'none');
+    $('.arrowdown').css('display', 'none');
+    $('.arrowdownwhite').css('display', 'none');
+    $('.book1').css('display', 'none');
+    $('.winx').css('display', 'none');
+  });
+  $('.books').click(function() {
+    $('.shape1a').css('display', 'none');
+    $('.souvenirproducts').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.vector4').css('display', 'none');
+    $('.dreamsproducts').css('display', 'none');
+    $('.texthistory1').css('display', 'block');
+    $('.arrowdown').css('display', 'block');
+    $('.arrowdownwhite').css('display', 'none');
+    $('.book1').css('display', 'none');
+    $('.winx').css('display', 'none');
   });
 });
 
 
-$(document).ready(function() {
+$(function() {
   $('.dreamproduct1').hover(function() {
     $('.dreamproduct1').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct2').hover(function() {
     $('.dreamproduct2').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct3').hover(function() {
     $('.dreamproduct3').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct4').hover(function() {
     $('.dreamproduct4').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct5').hover(function() {
     $('.dreamproduct5').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct6').hover(function() {
     $('.dreamproduct6').toggleClass('dreamproduct');
   });
-});
-
-
-$(document).ready(function() {
   $('.dreamproduct7').hover(function() {
     $('.dreamproduct7').toggleClass('dreamproduct');
+  });
+  $('.souvenirproduct1').hover(function() {
+    $('.souvenirproduct1').toggleClass('souvenirproduct');
+  });
+  $('.souvenirproduct2').hover(function() {
+    $('.souvenirproduct2').toggleClass('souvenirproduct');
+  });
+  $('.souvenirproduct3').hover(function() {
+    $('.souvenirproduct3').toggleClass('souvenirproduct');
   });
 });
 
 
-$(document).ready(function() {
+$(function() {
   $('.dreamproduct1').click(function() {
     $('.dreamsproducts').css('display', 'none');
     $('.containermain').css('display', 'none');
@@ -268,28 +284,143 @@ $(document).ready(function() {
     $('.productinfo1').css('display', 'block');
     $('.arrowblue').css('display', 'block');
   });
+  $('.dreamproduct2').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo2').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.dreamproduct3').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo3').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.dreamproduct4').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo4').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.dreamproduct5').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo5').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.dreamproduct6').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo6').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.dreamproduct7').click(function() {
+    $('.dreamsproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector').css('display', 'none');
+    $('.productinfo7').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.souvenirproduct1').click(function() {
+    $('.souvenirproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector4').css('display', 'none');
+    $('.productinfo8').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.souvenirproduct2').click(function() {
+    $('.souvenirproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector4').css('display', 'none');
+    $('.productinfo9').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
+  $('.souvenirproduct3').click(function() {
+    $('.souvenirproducts').css('display', 'none');
+    $('.containermain').css('display', 'none');
+    $('.points1').css('display', 'none');
+    $('.vector4').css('display', 'none');
+    $('.productinfo10').css('display', 'block');
+    $('.arrowblue').css('display', 'block');
+  });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.arrowblue').click(function() {
     $('.containermain').css('display', 'grid');
     $('.dreamsproducts').css('display', 'grid');
     $('.vector').css('display', 'block');
     $('.arrowblue').css('display', 'none');
     $('.productinfo1').css('display', 'none');
+    $('.productinfo2').css('display', 'none');
+    $('.productinfo3').css('display', 'none');
+    $('.productinfo4').css('display', 'none');
+    $('.productinfo5').css('display', 'none');
+    $('.productinfo6').css('display', 'none');
+    $('.productinfo7').css('display', 'none');
     $('.points1').css('display', 'flex');
     $('.arrowblue').css('display', 'none');
+    $('.productinfo8').css('display', 'none');
+    $('.productinfo9').css('display', 'none');
+    $('.productinfo10').css('display', 'none');
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.buy').click(function() {
     $('.welldone').fadeIn();
   });
 });
 
-$(document).ready(function() {
+$(function() {
   $('.close6').click(function() {
     $('.welldone').fadeOut();
+  });
+});
+
+
+$(function() {
+  $('.arrowdown').click(function() {
+    $('.texthistory1').fadeOut();
+    $('.book1').fadeIn(100);
+    $('.arrowdown').css('display', 'none');
+    $('.arrowdownwhite').css('display', 'block');
+  });
+  $('.arrowdownwhite').click(function() {
+    $('.book1').css('display', 'none');
+    $('.winx').fadeIn(100);
+    $('.arrowdownwhite').css('display', 'none');
+  });
+});
+
+$(function() {
+  $('.where').click(function() {
+    $('.shape3a').css('display', 'none');
+    $('.background2').css('display', 'block');
+    $('iframe').css('display', 'block');
+    $('.aboutwhere').css('display', 'block');
+    $('.formamain').css('display', 'none');
+    $('.bg2').css('display', 'none');
+  });
+  $('.who').click(function() {
+    $('.shape3a').css('display', 'none');
+      $('.background2').css('display', 'none');
+        $('.aboutwhere').css('display', 'none');
+    $('iframe').css('display', 'none');
+    $('.formamain').css('display', 'block');
+    $('.bg2').css('display', 'block');
   });
 });
